2074321838f96d33efe48ac2c8d1798084a98057
bmcq9802 <bmcq9802@gmail.com>
2022-04-19 23:15:09 +0900

    1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
 $(document).ready(function() {
     $('.pofol_link').slick({
-        Infinite: true,
-        slideToShow: 1,
+        infinite: true,
+        slideToShow: 3,
         speed: 100,
         drggable: true,
-        arrows: false,
+        arrows: true,
+        dots: true,
         autoplay: true,
         autoplaySpeed: 4000,
         pauseOnHover: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+
+                    slidesToShow: 1,
+                    arrows: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 739,
+                settings: {
+
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: false,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                }
+            }
+        ]
     });
 });
