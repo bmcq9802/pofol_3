23a51d8545cd9d99fadafbbbd678f642f35e7618
bmcq9802 <bmcq9802@gmail.com>
2022-04-19 10:07:21 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.pofol_link').slick({
+        Infinite: true,
+        slideToShow: 1,
+        speed: 100,
+        drggable: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: true,
+    });
+});
