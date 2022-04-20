1b49713d21f80955bcaf7caffaec2ea891f3f129
bmcq9802 <bmcq9802@gmail.com>
2022-04-20 18:35:27 +0900

    df

--- js/script.js
+++ js/script.js
@@ -1,32 +1,37 @@
 new Chart(
-  document.getElementById('myChart').getContext('2d'),
-  {
-    type: 'doughnut',
-    data: {
-      datasets: [{
-        data: [
-          50,
-          50
-        ],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)'
-        ]
-      }],
-      labels: [
-        "red",
-        "blue"
-      ]
-    },
-    options: {
-      rotation: 1 * Math.PI,
-      circumference: 1 * Math.PI,
-      responsive: true,
-      legend: false,
-      title: false,
-      animation: {
-        animateRotate: true
-      }
+    document.getElementById('myChart').getContext('2d'), {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [
+                    50,
+                    20,
+                    20,
+                    10
+                ],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    '#000',
+                    '#fff'
+                ]
+            }],
+            labels: [
+                "Coding",
+                "Workout",
+                "Music",
+                "Reading"
+            ]
+        },
+        options: {
+            rotation: 1 * Math.PI,
+            circumference: 1 * Math.PI,
+            responsive: true,
+            legend: false,
+            title: false,
+            animation: {
+                animateRotate: true
+            }
+        }
     }
-  }
 );
