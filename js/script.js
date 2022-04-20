ad856f7f052fc1ec38142681476ca4ed2155c918
bmcq9802 <bmcq9802@gmail.com>
2022-04-20 17:18:34 +0900

    1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+new Chart(
+  document.getElementById('myChart').getContext('2d'),
+  {
+    type: 'doughnut',
+    data: {
+      datasets: [{
+        data: [
+          50,
+          50
+        ],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)'
+        ]
+      }],
+      labels: [
+        "red",
+        "blue"
+      ]
+    },
+    options: {
+      rotation: 1 * Math.PI,
+      circumference: 1 * Math.PI,
+      responsive: true,
+      legend: false,
+      title: false,
+      animation: {
+        animateRotate: true
+      }
+    }
+  }
+);
